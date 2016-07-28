282cc9ba73005ed2e6b18da0019fce44bc10942a
Max <maxfooo@gmail.com>
2016-07-28 16:59:45 -0700

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -3,19 +3,27 @@ Created on Jul 20, 2016
 
 @author: maxr
 '''
-
+from tkinter import Tk
 from ExtractData import ExtractData
 from Utils import DNL, INL, quickPlot, quickNormal
+from PostProcessingUI import PostProcessingUI
 
-FILE_NAME = "dac_data_spec.csv"
 ADC_PRECISION = 24
 MAX_ADC_VALUE = 2**ADC_PRECISION-1
+SAMPLES_PER_CODE = 10
 MAX_VOLTAGE = 3.3
+CHIP_SPECS = [ADC_PRECISION, MAX_ADC_VALUE, SAMPLES_PER_CODE, MAX_VOLTAGE]
+
+
+FILE_NAME = "dac_data_spec.csv"
+FILE = None
 LSB = MAX_VOLTAGE / MAX_ADC_VALUE
 SPECIAL_CASE = True
+BREAK_WHEN_ERR = False
 
 if __name__ == '__main__':
-    ED = ExtractData(FILE_NAME, SPECIAL_CASE)
+    
+    ED = ExtractData(FILE_NAME, FILE, CHIP_SPECS, SPECIAL_CASE, BREAK_WHEN_ERR)
     avg = ED.averageSamples()
     dnl = []
     volt = []
@@ -25,6 +33,15 @@ if __name__ == '__main__':
     dnl = DNL(volt, LSB)
     print(dnl)
     quickPlot(dnl, ttl="DNL of 12 bit dac", xlbl="Code", ylbl="LSB")
+    quickPlot(volt,ylbl='Volt')
+    """
+
+    
+    root = Tk()
+    app = PostProcessingUI(master=root)
+    app.mainloop()
+    """
+    
     
     
     
