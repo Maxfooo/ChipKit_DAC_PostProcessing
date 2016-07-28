be084a1c5b181702aa3392fbcc28321f76163d2e
Max <maxfooo@gmail.com>
2016-07-27 17:24:31 -0700

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -7,14 +7,15 @@ Created on Jul 20, 2016
 from ExtractData import ExtractData
 from Utils import DNL, INL, quickPlot, quickNormal
 
-FILE_NAME = "dac_data.csv"
+FILE_NAME = "dac_data_spec.csv"
 ADC_PRECISION = 24
 MAX_ADC_VALUE = 2**ADC_PRECISION-1
 MAX_VOLTAGE = 3.3
 LSB = MAX_VOLTAGE / MAX_ADC_VALUE
+SPECIAL_CASE = True
 
 if __name__ == '__main__':
-    ED = ExtractData(FILE_NAME)
+    ED = ExtractData(FILE_NAME, SPECIAL_CASE)
     avg = ED.averageSamples()
     dnl = []
     volt = []
