23575d2578496a45ec77a74168c12c62931e74e9
Max <maxfooo@gmail.com>
2016-07-28 17:00:25 -0700

    Update ExtractData.py

--- ExtractData.py
+++ ExtractData.py
@@ -18,23 +18,25 @@ http://www.analog.com/media/en/technical-documentation/data-sheets/AD7780.pdf
 """
 import re
 
-BREAK_WHEN_ERR = False
-
-ADC_PRECISION = 24
-MAX_ADC_VALUE = 2**ADC_PRECISION-1
-SAMPLES_PER_CODE = 10
-MAX_VOLTAGE = 3.3
 ERR_0 = 'Too Many Samples'
 ERR_1 = 'Too Few Samples'
 ERR_2 = 'Repeat Code'
 ERR_3 = 'Out of Order Code'
 
-
 class ExtractData(object):
     
-    def __init__(self, fileName, specialCase=False):
+    def __init__(self, fileName=None, file=None, chipSpecs=[], specialCase=False, break_when_err=False):
         self.fileName = fileName
+        self.file = file
+        
+        self.adc_precision = chipSpecs[0]
+        self.max_adc_value = chipSpecs[1]
+        self.samples_per_code = chipSpecs[2]
+        self.max_voltage = chipSpecs[3]
+        
         self.specCase = specialCase
+        self.break_when_err = break_when_err
+        
         self.extractData()
         self.generateErrorLog()
     
@@ -43,56 +45,60 @@ class ExtractData(object):
         self.pswDict = {}
         prevCode = -1
         self.errorDict = {ERR_0 : [], ERR_1: [], ERR_2: [], ERR_3: []}
-        with open(self.fileName, 'r') as f:
-            lineNum = 0
-            sampleCount = SAMPLES_PER_CODE-1
-            for line in f:
-                word = line.split(',')
-                
-                if re.search('[a-zA-Z]', word[0]):
-                    continue
-                
-                word[0] = int(word[0])
-                
-                if self.specCase:
-                    word[1] = word[1][1:] # remove first bit for special case
-                    _max_adc_value = 2**(ADC_PRECISION-1)-1
-                    voltage = (float(int(word[1], 2)) / _max_adc_value) * MAX_VOLTAGE
-                else:                
-                    voltage = (float(int(word[1], 2)) / MAX_ADC_VALUE) * MAX_VOLTAGE
-                
-                if prevCode == word[0]:
-                    if sampleCount >= SAMPLES_PER_CODE:
-                        self.errorDict[ERR_0].append('Line: {0}, Entry: {1}'.format(lineNum, line))
-                        if BREAK_WHEN_ERR:
-                            break
-                    self.sampleDict[word[0]].append(voltage)
-                    self.pswDict[word[0]].append(word[2])
-                    sampleCount += 1
-                        
-                elif prevCode == word[0] - 1:
-                    if sampleCount != SAMPLES_PER_CODE-1:
-                        self.errorDict[ERR_1].append('Line: {0}, Entry: {1}'.format(lineNum, line))
-                        if BREAK_WHEN_ERR:
-                            break
-                    if word[0] in self.sampleDict.keys():
-                        self.errorDict[ERR_2].append('Line: {0}, Entry: {1}'.format(lineNum, line))
-                        if BREAK_WHEN_ERR:
-                            break
-                    self.sampleDict[word[0]] = [voltage]
-                    self.pswDict[word[0]] = [word[2]]
-                    sampleCount = 0
-                    
-                else:
-                    self.errorDict[ERR_3].append('Line: {0}, Entry: {1}'.format(lineNum, line))
-                    if BREAK_WHEN_ERR:
+        if self.fileName == None:
+            f = self.file
+        else:
+            f = open(self.fileName, 'r')
+        #with open(self.fileName, 'r') as f:
+        lineNum = 0
+        sampleCount = self.samples_per_code-1
+        for line in f:
+            word = line.split(',')
+            
+            if re.search('[a-zA-Z]', word[0]):
+                continue
+            
+            word[0] = int(word[0])
+            
+            if self.specCase:
+                word[1] = word[1][1:] # remove first bit for special case
+                _max_adc_value = 2**(self.adc_precision-1)-1
+                voltage = (float(int(word[1], 2)) / _max_adc_value) * self.max_voltage
+            else:                
+                voltage = (float(int(word[1], 2)) / self.max_adc_value) * self.max_voltage
+            
+            if prevCode == word[0]:
+                if sampleCount >= self.samples_per_code:
+                    self.errorDict[ERR_0].append('Line: {0}, Entry: {1}'.format(lineNum, line))
+                    if self.break_when_err:
                         break
-                    self.sampleDict[word[0]] = [voltage]
-                    self.pswDict[word[0]] = [word[2]]
-                    sampleCount = 0
+                self.sampleDict[word[0]].append(voltage)
+                self.pswDict[word[0]].append(word[2])
+                sampleCount += 1
+                    
+            elif prevCode == word[0] - 1:
+                if sampleCount != self.samples_per_code-1:
+                    self.errorDict[ERR_1].append('Line: {0}, Entry: {1}'.format(lineNum, line))
+                    if self.break_when_err:
+                        break
+                if word[0] in self.sampleDict.keys():
+                    self.errorDict[ERR_2].append('Line: {0}, Entry: {1}'.format(lineNum, line))
+                    if self.break_when_err:
+                        break
+                self.sampleDict[word[0]] = [voltage]
+                self.pswDict[word[0]] = [word[2]]
+                sampleCount = 0
                 
-                prevCode = word[0]
-                lineNum += 1
+            else:
+                self.errorDict[ERR_3].append('Line: {0}, Entry: {1}'.format(lineNum, line))
+                if self.break_when_err:
+                    break
+                self.sampleDict[word[0]] = [voltage]
+                self.pswDict[word[0]] = [word[2]]
+                sampleCount = 0
+            
+            prevCode = word[0]
+            lineNum += 1
                     
         f.close()
         
