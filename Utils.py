c1d7afd8e7c12dbee30649246b16bcb65ab6e328
Max <maxfooo@gmail.com>
2016-08-05 09:18:46 -0700

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -8,6 +8,9 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import numpy as np
 from statistics import mean, stdev
+import sys
+import glob
+import serial
 
 def quickPlot(x,y=None, ttl='DAC Voltage', xlbl = 'Time (sec)', ylbl='VDC (V)'):
     if y == None:
@@ -62,4 +65,35 @@ def INL(volt, lsb):
         else:
             inl.append(0)
     return inl
-        
+
+
+def serial_ports():
+    """ 
+    @Source: http://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+    Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
